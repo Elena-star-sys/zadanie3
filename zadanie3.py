07d8d43ae25b589098592f41879a22a31a21e493
Elena-star-sys <elenashulgaelena@gmail.com>
2024-07-21 15:56:33 +0300

    Add files via upload

---
+++ zadanie3.py
@@ -0,0 +1,5 @@
+domzad = 'Курс : Python'
+zadahi = " Количество задач"
+chasi = "Затрачено часов"
+vremya = "Среднее время выполнения"
+print(domzad, zadahi, ":" "12", chasi,":" "1,5", vremya,":""0,125" "часа")
